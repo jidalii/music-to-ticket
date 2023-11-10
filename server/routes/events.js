cada47a4d14166757f4eb459e59e67c619127514
JackMuuu <ymmu@bu.edu>
2023-11-10 01:57:11 -0500

    events.js edited and events.ejs added
    - One function and one http method added in events.js\n- events.ejs added as an alternative way to show the events info; used by adjusting one place in events.js

--- server/routes/events.js
+++ server/routes/events.js
@@ -9,6 +9,19 @@ const Spotify = require("../model/gallery");
 const ticketmaster_root_url = "https://app.ticketmaster.com/discovery/v2/"
 const API_KEY = process.env.TICKETMASTER_CLIENT_ID
 
+// Function to get artist names from MongoDB
+async function getArtistNamesFromDB() {
+    try {
+        // Fetch all artist entries and extract names
+        const artists = await Spotify.find().select('artist.name -_id');
+        return artists.map(item => item.artist.map(artist => artist.name)).flat();  //flatten into a single array of artist names
+    } catch (error) {
+        console.error('Error fetching artist names from MongoDB:', error);
+        throw error;
+    }
+}
+
+
 router.get('/event/:artist', async (req, res) => {
 
     try{
@@ -31,5 +44,28 @@ router.get('/event/:artist', async (req, res) => {
     }
 
 })
+
+
+// New route to get events for all artists in MongoDB
+router.get('/', async (req, res) => {
+    try {
+        const artistNames = await getArtistNamesFromDB();
+        const eventsPromises = artistNames.map(name => axios.get(`${ticketmaster_root_url}events.json?apikey=${API_KEY}&keyword=${encodeURIComponent(name)}`));
+        const eventsResponses = await Promise.allSettled(eventsPromises); //wait for all API requests to resolved or rejected
+
+        const events = eventsResponses
+            .filter(response => response.status === 'fulfilled')
+            .map(response => response.value.data._embedded?.events ?? [])  //handle with no event was found // ?? [] provide an empty array if _embedded is undefined or null
+            .flat(); //flatten into a single array
+
+        res.json(events); // another res way to pass to events.ejs "res.render('events', { events: events });" for displaying filtered important info instead of all info
+    } catch (error) {
+        console.error('Error fetching events for artists:', error);
+        res.status(500).send('An error occurred while fetching events');
+    }
+});
+
+//:)
+router.get('/fun', (req, res) => {res.status(418).send("I'm a teapot"); });
 
 module.exports = router;
