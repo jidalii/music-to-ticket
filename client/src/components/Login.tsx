049170ef9715f8a51b7d1781321f4ae12b00cb56
cccuirq <123427931+cccuirq@users.noreply.github.com>
2023-11-21 17:05:34 -0500

    shishi

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,7 +3,7 @@ import '../tailwind.css'
 
 function Login() {
   return (
-    <div className='flex h-screen justify-center items-center' >
+    <div id = "main" className='flex h-screen justify-center items-center' >
       <div className='text-center'>
         <h1 className="text-6xl text-spotify-green mb-4">
           Welcome to Music to Ticket
