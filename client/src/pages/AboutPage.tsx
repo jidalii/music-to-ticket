b80107fc7ba2dbf64a73fdd67eac2662933d2406
cccuirq <123427931+cccuirq@users.noreply.github.com>
2023-11-26 21:41:29 -0500

    finish about page

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -2,6 +2,8 @@ import '../AboutPage.css'
 import '../script.js'
 import xin from '../images/xin.jpeg';
 import try2 from '../images/try.jpeg';
+import pll from '../images/playlist.jpg';
+import ticket from '../images/ticket.jpg';
 import frontimg from '../images/frontimg.jpg';
 
 function AboutPage() {
@@ -22,24 +24,52 @@ function AboutPage() {
             </div>
         </header>
 
-
+        <h1 id = "h11">
+            <p>Create A Easier Way to</p>
+            <p>Link you and the One you Love</p>
+        </h1>
         <main id = "main">
             <section id="section1" class="section">
-                <img src={frontimg} alt = "Somepic" />
-                <h2>Who We Are</h2>
-                <p>Your Company is [a brief description of yourself or your organization]. [Provide some background, such as when it was founded, the mission, and the values.]</p>
+                <img src={ticket} width="600" height="400" alt = "Somepic" />
+                <section class="textsection">
+                    <h2>Who We Are</h2>
+                    <p>We are four student from boston university, providing website for you to find recent concert of you favorite artest by just one click.</p>
+                </section>
             </section>
 
+        <h1 id = "h12">
+            <p id ="unreal">Sounds unrealiable?</p>
+            <p id = "new">Even NO need to create new account</p>
+        </h1>
+
             <section id= "section3" class="section">
-                <h2>What We Offer</h2>
-                <p>[Highlight the key products, services, or content that you provide. Describe how you add value or solve problems for your audience.]</p>
-                <img src={try2}  width="600" height="800" alt = "Somepic" />
+                <section class="textsection">
+                    <h2>What We Offer</h2>
+                    <p>
+                    <ol>
+                        <li>1. Login with your spotify</li>
+                        <li>2. Get information about singers and artest you like and Link to ticketmaster</li>
+                        <li>3. Check if your love have concert recently</li>
+                        <li>4. directly link to ticketmaster and by your ticket!</li>
+                    </ol>
+                    </p>
+                </section>
+                <img id ="rightimg" src={pll}  width="600" height="800" alt = "Somepic"/>
             </section>
 
             <section id="section4" class="section">
                 <img src={xin} width="600" height="800" alt = "Somepic" />
-                <h2>Join Our Community</h2>
-                <p>[If applicable, invite visitors to join your community, subscribe to newsletters, or follow your updates on social media.]</p>
+                <section>
+                    <h2>Join Our Community</h2>
+                    <p id = "login">Login in with your spotify Now!</p>
+                    <p id = "blogin">Try what I've mentioned above, and Enjoy your time.</p>
+                    <button id="loginbutton"
+                        className="bg-spotify-green text-white px-5 py-2.5 inline-block text-lg m-1 cursor-pointer rounded-full font-bold"
+                        onClick={() => window.location.href='http://localhost:8000/auth/spotify'}
+                         >
+                        Login with Spotify
+                     </button>
+                </section>
             </section>
         </main>
     </body>
