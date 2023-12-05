c56e653411bd10852f9c1965f828c0017a2e1b35
cccuirq <123427931+cccuirq@users.noreply.github.com>
2023-12-05 13:51:28 -0500

    update nav

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+// import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
 import '../tailwind.css'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 
+interface User {
+  username: string,
+  email: string,
+  avatar: string
+}
 
 function NavBar() {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+        try {
+            const response = await axios.get('http://localhost:8000/auth/user-data', { withCredentials: true });
+            setUser(response.data);
+            // USER_ID = response.data
+            console.log(response.data);
+        } catch (error) {
+            console.error('Error fetching user data:', error);
+        }
+    };
+
+    fetchUserData();
+    }, []);
+
+
     return (
         <nav id = "nav" className="fixed top-0 w-full z-50 bg-white shadow-md">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -18,10 +43,21 @@ function NavBar() {
               <Link className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium" to="/profile">Profile</Link>
               <Link className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium" to="/about">About</Link>
             </div>
+            {user ? (
             <Avatar>
               <AvatarImage  />
-              <AvatarFallback>CN</AvatarFallback>
+              <AvatarFallback>
+                <img src={user.avatar} alt="CN"/>
+              </AvatarFallback>
             </Avatar>
+            ):(
+              <Avatar>
+              <AvatarImage  />
+              <AvatarFallback>
+               CN
+              </AvatarFallback>
+              </Avatar>
+            )}
           </div>
         </div>
       </nav>
